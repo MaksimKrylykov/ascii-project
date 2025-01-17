9850c6912a8aeadd48de0e643f4f00c6bc422caf
Maksim Krylykov <maksimkrylykov@gmail.com>
2025-01-16 21:21:13 -0500

    first stage

---
+++ main.py
@@ -0,0 +1,70 @@
+import sys
+
+FIRST_SYMBOL = ' '
+LAST_SYMBOL = '~'
+
+CHAR_HEIGHT = 8
+
+
+def fix_str(string):
+    fixed_str = ""
+    i = 0
+    while i < len(string):
+        if i + 1 < len(string) and string[i] == '\\' and string[i + 1] == 'n':
+            fixed_str += '\n'
+            i += 2
+            continue
+        
+        fixed_str += input_text[i]
+        i += 1
+    return fixed_str
+
+
+def ascii_art(text):
+    font_file = open("./fonts/standard.txt")
+    lines = font_file.readlines()
+    
+    ascii_map = dict()
+    current_line = 1
+    for i in range(ord(FIRST_SYMBOL), ord(LAST_SYMBOL) + 1):
+        art = [lines[i][:-1] for i in range(current_line, current_line + CHAR_HEIGHT)]
+        
+        ascii_map[chr(i)] = art
+        
+        current_line += CHAR_HEIGHT + 1
+
+    matrix_art = []
+    current_line = 0
+
+    for char in text:
+        if char == '\n':
+            current_line = len(matrix_art)
+            matrix_art.append('')
+            continue
+
+        if not ascii_map.get(char):
+            continue
+
+        while len(matrix_art) < current_line + CHAR_HEIGHT:
+            matrix_art.append('')
+
+        for i in range(current_line, current_line + CHAR_HEIGHT):
+            matrix_art[i] += ascii_map[char][i - current_line]
+
+    final_art = ''
+
+    for i in range(len(matrix_art)):
+        final_art += matrix_art[i]
+        final_art += '\n'
+
+    return final_art
+
+
+if __name__ == '__main__':
+    
+    if len(sys.argv) > 1:
+        input_text = sys.argv[1].strip()
+
+        fixed_text = fix_str(input_text)
+        
+        print(ascii_art(fixed_text), end='')
